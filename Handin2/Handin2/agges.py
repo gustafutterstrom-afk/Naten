1b8f169ac08582cc0daac0c5953923000047fc14
Your Name <you@example.com>
2026-05-05 13:28:36 +0200

    dsds

--- Handin2/Handin2/agges.py
+++ Handin2/Handin2/agges.py
@@ -1,12 +1,14 @@
 import networkx as nx
 import numpy as np
 from scipy.io import loadmat
+import cvxpy as cv
 
 B = np.loadtxt('traffic.mat', delimiter=',')
 print(B.shape)
 le = np.loadtxt('traveltime.mat', delimiter=',')
 print(le.shape)
 cap = np.loadtxt('capacities.mat', delimiter=',')
+flo = np.loadtxt('flow.mat', delimiter=',')
 #W = np.zeros((B.shape[0], B.shape[0]))
 G = nx.DiGraph()
 for i in range(B.shape[1]):  
@@ -26,9 +28,41 @@ for i in range(B.shape[1]):
 #print(W)
 #G = nx.DiGraph(W, capacity=cap)
 shortestp = nx.shortest_path(G, source=0, target=16)
-shortest = nx.shortest_path_length(G, source=0, target=16)
-print(shortest)
-print(shortestp)
+shortest = nx.shortest_path_length(G, source=0, target=16) #a
+#print(shortest)
+#print(shortestp)
 
-mflow, dict = nx.maximum_flow(G, 0, 16)
+mflow, dict = nx.maximum_flow(G, 0, 16) #b
 print(mflow)
+
+mu = B@flo
+print(mu) #c
+
+
+v = np.zeros(B.shape[0])
+v[0] = 22447
+v[-1] = -22447
+fe = cv.Variable(B.shape[1])
+# 2. Define Constraints
+constraints = [
+    B @ fe == v,
+    fe >= 0,
+    fe <= cap
+]
+
+# SAFE convex expression
+cost = cv.sum(
+    cv.multiply(
+        le * cap,
+        cv.inv_pos(1 - cv.multiply(fe, 1 / cap)) - 1
+    )
+)
+
+objective = cv.Minimize(cost)
+
+problem = cv.Problem(objective, constraints)
+
+print(problem.solve())
+print(problem.status)
+
+
