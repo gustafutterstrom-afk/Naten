0f229b7875ec6e5c9a7b0930b178e213e845c632
Gustaf Utterström <gustafutterstrom@MacBook-Pro-som-tillhor-Gustaf.local>
2026-05-06 16:20:00 +0200

    a

--- Handin2/Handin2/code.py
+++ Handin2/Handin2/code.py
@@ -10,10 +10,10 @@ cap = np.loadtxt('Handin2/Handin2/capacities.mat')
 flow = np.loadtxt('Handin2/Handin2/flow.mat', delimiter=',')
 #print(B.shape) # 17,28
 #print(cap.shape[0]) # 28 rader
-print(B.shape[0])# 17 rader
+#print(B.shape[0])# 17 rader
 #print(B.shape[1]) # 28 colonner
 #print(l.shape[0]) #28 lång
-print(flow.shape[0]) # 28
+#print(flow.shape[0]) # 28
 
 G = nx.Graph()
 for j in range (B.shape[1]):
@@ -27,33 +27,32 @@ for j in range (B.shape[1]):
     if a is not None and b is not None:
         G.add_edge(a,b,weight=l[j], capacity = cap[j])
 
-
-shortest = nx.shortest_path_length(G,source=0,target=16)
+shortest = nx.shortest_path_length(G,source=0,target=16, weight='weight')
 shortestP = nx.shortest_path(G,source=0,target =16, weight='weight')
 print(f"the shortest path:{shortest}")
 print(f'The path:{shortestP}') # Nollidexering 
-#### Execercise b)
-maxF, dict = nx.maximum_flow(G, 0,16)
 
+#### Execercise b)#####
+maxF, dict = nx.maximum_flow(G, 0,16)
 print(f'Maxflow - the sum of the first split:{maxF}')
 
-#### Execercise c)###
+#### Execercise c)#####
 mu = np.matmul(B,flow)
 print(mu) # external positivt innebär inflow (-1 i B matrisen är in)
 
-#### Execercise d)###
-v = np.zeros(B.shape[0])
+#### Execercise d)######
+v = np.zeros(B.shape[0]) # External flow vecotor
 v[0] = mu[0]
 v[-1] = -mu[0]
 fe = cp.Variable(B.shape[1])
-# 2. Define Constraints
+
+# Constraints
 constraints = [
-    B @ fe == v,
+    B @ fe == v, # External flow 
     fe >= 0,
     fe <= cap
 ]
-
-# SAFE convex expression
+# expression
 cost = cp.sum(
     cp.multiply(
         l * cap,
@@ -63,13 +62,12 @@ cost = cp.sum(
 
 objective_soc = cp.Minimize(cost)
 problem_soc = cp.Problem(objective_soc, constraints)
-
-
+print("Social optimum")
 print(problem_soc.solve())
+print("Social optimum flow")
 print(fe.value)
 
-
-### e)
+### e)######
 fe2 = cp.Variable(B.shape[1])
 cost_war = cp.sum(cp.multiply(cp.multiply(-cap,l),cp.log(1-fe2/cap)))
 objective_war = cp.Minimize(cost_war)
@@ -79,11 +77,13 @@ constraints = [
     fe2 <= cap
 ]
 problem_war = cp.Problem(objective_war, constraints)
+print("Wardop optimum")
 print(problem_war.solve())
+print("Wardrop flow-values")
 print(fe2.value)
 fe_star=fe.value
 
-##f
+##f)#####
 fe3 =cp.Variable(B.shape[1])
 cost_war_toll = cp.sum(cp.multiply(cp.multiply(-cap,l),cp.log(1-fe3/cap))+cp.multiply(fe3,cp.multiply(l,fe_star))/cp.multiply(cap,cp.square(1-fe_star/cap)) )
 objective_war_toll = cp.Minimize(cost_war_toll)
@@ -93,7 +93,9 @@ constraints = [
     fe3 <= cap
 ]
 problem_war_toll = cp.Problem(objective_war_toll, constraints)
+print("Wardrop with tolls")
 print(problem_war_toll.solve())
+print("Wardrop flow with tolls")
 print(fe3.value)
               
 ## g###
@@ -113,7 +115,7 @@ constraints = [
 ]
 problem_soc_2 = cp.Problem(objective_soc_2, constraints)
 print(problem_soc_2.solve())
-print("soscial optimum:")
+print("Social optimum:")
 print(fe4.value)
 fe_star2=fe4.value
 
@@ -126,5 +128,6 @@ constraints = [
     fe5 <= cap
 ]
 problem_war_toll2 = cp.Problem(objective_war_toll2, constraints)
+print("with tolls")
 print(problem_war_toll2.solve())
 print(fe5.value)
